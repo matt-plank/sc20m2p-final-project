340fbb4773e82ddded1d42733840558d314dfd4f
matt-plank <matthew_plank@outlook.com>
2023-03-27 19:20:39 +0100

    Refactored into encoder and decoder to combine images

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import glob
+
 import matplotlib.pyplot as plt
 import numpy as np
 from keras import layers, models
@@ -5,60 +7,95 @@ from keras.utils import img_to_array, load_img
 from skimage.transform import resize
 
 
-def create_model() -> models.Sequential:
+def create_model() -> tuple[models.Sequential, models.Sequential, models.Sequential]:
     """Create a model for re-creating an image."""
     model = models.Sequential()
-    model.add(layers.Conv2D(32, (3, 3), activation="relu", input_shape=(128, 128, 3)))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation="relu"))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(128, (3, 3), activation="relu"))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(128, (3, 3), activation="relu"))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Flatten())
-    model.add(layers.Dropout(0.5))
+    encoder_layers = [
+        layers.Conv2D(32, (3, 3), activation="relu", input_shape=(128, 128, 3)),
+        layers.MaxPooling2D((2, 2)),
+        layers.Conv2D(64, (3, 3), activation="relu"),
+        layers.MaxPooling2D((2, 2)),
+        layers.Conv2D(128, (3, 3), activation="relu"),
+        layers.MaxPooling2D((2, 2)),
+        layers.Conv2D(128, (3, 3), activation="relu"),
+        layers.MaxPooling2D((2, 2)),
+        layers.Flatten(),
+        layers.Dropout(0.5),
+        layers.Dense(1024, activation="relu"),
+        layers.Dense(512, activation="relu"),
+        layers.Dense(256, activation="relu"),
+        layers.Dense(128, activation="relu"),
+        layers.Dense(64, activation="relu"),
+    ]
 
-    model.add(layers.Dense(1024, activation="relu"))
-    model.add(layers.Dense(512, activation="relu"))
-    model.add(layers.Dense(256, activation="relu"))
-    model.add(layers.Dense(128, activation="relu"))
-    model.add(layers.Dense(64, activation="relu"))
+    # Re-create the image
+    decoder_layers = [
+        layers.Dense(256, activation="relu"),
+        layers.Dense(512, activation="relu"),
+        layers.Dense(128 * 128 * 3, activation="sigmoid"),
+        layers.Reshape((128, 128, 3)),
+    ]
 
-    model.add(layers.Dense(256, activation="relu"))
-    model.add(layers.Dense(512, activation="relu"))
-    model.add(layers.Dense(128 * 128 * 3, activation="sigmoid"))
-    model.add(layers.Reshape((128, 128, 3)))
-    model.compile(loss="mse", optimizer="adam", metrics=["acc"])
+    model = models.Sequential([*encoder_layers, *decoder_layers])
+    encoder = models.Sequential(encoder_layers)
+    decoder = models.Sequential(decoder_layers)
 
-    return model
+    model.compile(optimizer="adam", loss="mse", metrics=["accuracy"])
+
+    return model, encoder, decoder
 
 
-def load_image(image_path: str) -> np.ndarray:
+def load_image(image_path: str, expand: bool = False) -> np.ndarray:
     """Load an image."""
     img = load_img(image_path)
     img = img_to_array(img)
     img = resize(img, (128, 128))
+    img = img / 255.0
+
+    if expand:
+        img = np.expand_dims(img, axis=0)
 
     return img
 
 
+# Use load_image() to load images from "trainingImages/" and return as a numpy array
+def load_images() -> np.ndarray:
+    """Load images from a directory."""
+    # Array called image_paths that uses glob to find all images in the directory "trainingImages/"
+    image_paths = glob.glob("trainingImages/*")
+
+    images = []
+
+    for image_path in image_paths:
+        img = load_image(image_path)
+        images.append(img)
+
+    return np.array(images)
+
+
+# Train the model from create_model() to create "testImage.jpeg"
 def main():
-    # Train the model created by create_model() to re-create the image loaded by load_image()
-    model = create_model()
-    img = load_image("testImage.jpeg")
-    img = resize(img, (128, 128))
-    img = img / 255.0
-    img = np.expand_dims(img, axis=0)
-    model.fit(img, img, epochs=10, batch_size=1)
+    # Load the images
+    img_1 = load_image("trainingImages/testImage.jpeg", expand=True)
+    img_2 = load_image("trainingImages/testCombine.jpg", expand=True)
 
-    print(img)
+    training_images = load_images()
 
-    # Predict the image
-    prediction = model.predict(img)
+    # Create the model
+    model, encoder, decoder = create_model()
+    model.fit(training_images, training_images, epochs=10, batch_size=1)
 
-    # Plot the image
-    plt.imshow(prediction[0])
+    # Encode the images
+    encoded = encoder.predict(img_1)
+    encoded_combine = encoder.predict(img_2)
+
+    combined = (encoded + encoded_combine) / 2
+
+    # Decode the combined images
+    result = decoder.predict(combined)
+
+    # Plot decoded as an image
+    plt.imshow(result[0])
     plt.show()
 
 
