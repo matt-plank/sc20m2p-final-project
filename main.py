afc612f094002ecc3b0d8460e96491bc7ecab3eb
matt-plank <matthew_plank@outlook.com>
2023-03-29 19:05:05 +0100

    Changed model structure and image dimensions (for training speed during exploratory phase)

--- main.py
+++ main.py
@@ -10,31 +10,29 @@ from skimage.transform import resize
 def create_model() -> tuple[models.Sequential, models.Sequential, models.Sequential]:
     """Create a model for re-creating an image."""
     model = models.Sequential()
+
+    # The layers of a tensorflow and keras model designed for processing 32x32 images
     encoder_layers = [
-        layers.Conv2D(32, (3, 3), activation="relu", input_shape=(128, 128, 3)),
-        layers.MaxPooling2D((2, 2)),
-        layers.Conv2D(64, (3, 3), activation="relu"),
-        layers.MaxPooling2D((2, 2)),
-        layers.Conv2D(128, (3, 3), activation="relu"),
-        layers.MaxPooling2D((2, 2)),
-        layers.Conv2D(128, (3, 3), activation="relu"),
-        layers.MaxPooling2D((2, 2)),
+        layers.Conv2D(32, (3, 3), activation="relu", padding="same", input_shape=(32, 32, 3)),
+        layers.MaxPooling2D((2, 2), padding="same"),
+        layers.Conv2D(64, (3, 3), activation="relu", padding="same"),
+        layers.MaxPooling2D((2, 2), padding="same"),
+        layers.Conv2D(128, (3, 3), activation="relu", padding="same"),
+        layers.MaxPooling2D((2, 2), padding="same"),
+        layers.Conv2D(256, (3, 3), activation="relu", padding="same"),
+        layers.MaxPooling2D((2, 2), padding="same"),
+        layers.Conv2D(512, (3, 3), activation="relu", padding="same"),
+        layers.MaxPooling2D((2, 2), padding="same"),
         layers.Flatten(),
-        layers.Dropout(0.5),
-        layers.Dense(1024, activation="relu"),
-        layers.Dense(512, activation="relu"),
-        layers.Dense(256, activation="relu"),
         layers.Dense(128, activation="relu"),
-        layers.Dense(64, activation="relu"),
-        layers.Dense(32, activation="relu"),
     ]
 
     # Re-create the image
     decoder_layers = [
         layers.Dense(256, activation="relu"),
         layers.Dense(512, activation="relu"),
-        layers.Dense(128 * 128 * 3, activation="sigmoid"),
-        layers.Reshape((128, 128, 3)),
+        layers.Dense(32 * 32 * 3, activation="sigmoid"),
+        layers.Reshape((32, 32, 3)),
     ]
 
     model = models.Sequential([*encoder_layers, *decoder_layers])
@@ -46,11 +44,11 @@ def create_model() -> tuple[models.Sequential, models.Sequential, models.Sequent
     return model, encoder, decoder
 
 
-def load_image(image_path: str, expand: bool = False) -> np.ndarray:
+def load_image(image_path: str, target_size: tuple[int, int], expand: bool = False) -> np.ndarray:
     """Load an image."""
     img = load_img(image_path)
     img = img_to_array(img)
-    img = resize(img, (128, 128))
+    img = resize(img, target_size)
     img = img / 255.0
 
     if expand:
@@ -59,17 +57,24 @@ def load_image(image_path: str, expand: bool = False) -> np.ndarray:
     return img
 
 
-def load_images() -> np.ndarray:
-    """Load images from a directory."""
+def load_images(target_size: tuple[int, int], augment: bool = False) -> np.ndarray:
+    """Load images from the trainingImages folder.
+
+    Args:
+        augment: Whether to augment the images by flipping and rotating them.
+    """
     image_paths = glob.glob("trainingImages/*")
 
     images = []
 
     for image_path in image_paths:
-        img = load_image(image_path)
+        img = load_image(image_path, target_size=target_size)
         images.append(img)
+
+        if not augment:
+            continue
+
         images.append(np.flip(img, axis=0))
-        images.append(np.flip(img, axis=1))
 
         for _ in range(3):
             img = np.rot90(img)
@@ -83,10 +88,10 @@ def load_images() -> np.ndarray:
 
 def main():
     # Load the images
-    img_1 = load_image("trainingImages/testImage.jpeg", expand=True)
-    img_2 = load_image("trainingImages/testCombine.jpg", expand=True)
+    img_1 = load_image("trainingImages/testImage.jpeg", (32, 32), expand=True)
+    img_2 = load_image("trainingImages/testCombine.jpg", (32, 32), expand=True)
 
-    training_images = load_images()
+    training_images = load_images((32, 32), augment=True)
 
     # Create the model
     model, encoder, decoder = create_model()
@@ -94,14 +99,14 @@ def main():
     try:
         model.load_weights("model.h5")
     except OSError:
-        model.fit(training_images, training_images, epochs=5, batch_size=1)
+        model.fit(training_images, training_images, epochs=50, batch_size=32)
         model.save("model.h5")
 
     # Encode the images
     encoded = encoder.predict(img_1)
     encoded_combine = encoder.predict(img_2)
 
-    combined = (encoded + encoded_combine) / 2
+    combined = encoded * 0.5 + encoded_combine * 0.5
 
     # Decode the combined images
     result = decoder.predict(combined)
