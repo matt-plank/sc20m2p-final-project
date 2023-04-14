fc384f068b42d318fa0d26445bf9bb4ce5fdd656
matt-plank <matthew_plank@outlook.com>
2023-04-14 16:45:41 +0100

    Updated plotting

--- transferEngine/main.py
+++ transferEngine/main.py
@@ -1,6 +1,8 @@
-from typing import Tuple
+from typing import Any, Tuple
 
 import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
 
 from transferEngine.images import image_dataset_factory
 from transferEngine.models import model_factory
@@ -12,60 +14,64 @@ MODEL_TRAINING_BATCH_SIZE: int = 16
 IMAGE_SHAPE: Tuple[int, int, int] = (28, 28, 3)
 
 
-def plot(img_1, img_2, decoded_1, decoded_2, decoded_combined, training_history):
-    """Plot the results of the model.
+def plot_image_results(img_1, img_2, decoded_1, decoded_2, combined) -> Figure:
+    """Plot the results of the model comparing the original images and the decoded images.
 
     Args:
         img_1: The first image.
         img_2: The second image.
         decoded_1: The first decoded image.
         decoded_2: The second decoded image.
-        decoded_combined: The combined decoded image.
+        combined: The combined image.
+    """
+    # Create a 2x3 grid of plots
+    fig: Figure
+    axes: Any
+    fig, axes = plt.subplots(2, 3)
+
+    # Disable axes for all plots
+    for ax in axes.flat:
+        ax.axis("off")
+
+    # Plot the original images on the first row
+    axes[0, 0].imshow(img_1)
+    axes[0, 0].set_title("Original Image 1")
+
+    axes[0, 1].imshow(img_2)
+    axes[0, 1].set_title("Original Image 2")
+
+    # Plot the decoded images on the second row
+    axes[1, 0].imshow(decoded_1)
+    axes[1, 0].set_title("Decoded Image 1")
+
+    axes[1, 1].imshow(decoded_2)
+    axes[1, 1].set_title("Decoded Image 2")
+
+    # Plot the combined image on the second row
+    axes[1, 2].imshow(combined)
+    axes[1, 2].set_title("Combined Image")
+
+    return fig
+
+
+def plot_training_history(training_history) -> Figure:
+    """Plot the training history of the model.
+
+    Args:
         training_history: The training history of the model.
     """
-    # Create a figure
-    fig = plt.figure()
-    plt.tight_layout()
+    # Create a new figure
+    fig = plt.figure(figsize=(10, 5))
 
-    # Top row in 3x3 grid
-    top_row = plt.subplot2grid((4, 3), (0, 0), colspan=3)
-    blank_row = plt.subplot2grid((4, 3), (1, 0), colspan=3)
-    first_image = plt.subplot2grid((4, 3), (2, 0))
-    second_image = plt.subplot2grid((4, 3), (2, 1))
-    decoded_first_image = plt.subplot2grid((4, 3), (3, 0))
-    decoded_second_image = plt.subplot2grid((4, 3), (3, 1))
-    decoded_combined_image = plt.subplot2grid((4, 3), (3, 2))
-    empty_slot = plt.subplot2grid((4, 3), (2, 2))
+    ax1 = fig.add_subplot(111)
+    ax1.plot(training_history.history["accuracy"])
+    ax1.plot(training_history.history["val_accuracy"])
+    ax1.set_title("Model Accuracy")
+    ax1.set_ylabel("Accuracy")
+    ax1.set_xlabel("Epoch")
+    ax1.legend(["Training", "Validation"])
 
-    # Plot accuracy from training_history in the top row
-    top_row.plot(training_history.history["accuracy"])
-    top_row.plot(training_history.history["val_accuracy"])
-
-    # Turn off axes for the middle row, last column
-    empty_slot.axis("off")
-    blank_row.axis("off")
-
-    # Show original images
-    first_image.imshow(img_1[0])
-    second_image.imshow(img_2[0])
-    decoded_first_image.imshow(decoded_1[0])
-    decoded_second_image.imshow(decoded_2[0])
-    decoded_combined_image.imshow(decoded_combined[0])
-
-    # Title each subplot
-    top_row.set_title("Model Accuracy")
-    top_row.set_ylabel("Accuracy")
-    top_row.set_xlabel("Epoch")
-    top_row.legend(["Train", "Validation"], loc="upper left")
-
-    first_image.set_title("Original A")
-    second_image.set_title("Original B")
-    decoded_first_image.set_title("Decoded A")
-    decoded_second_image.set_title("Decoded B")
-    decoded_combined_image.set_title("Decoded Combined")
-
-    # Plot decoded as an image
-    plt.savefig("model.tf/results.png")
+    return fig
 
 
 def main():
@@ -92,7 +98,9 @@ def main():
 
     decoded_1, decoded_2, decoded_combined = model.encode_and_combine(img_1, img_2, 0.5)
 
-    plot(img_1, img_2, decoded_1, decoded_2, decoded_combined, training_history)
+    # Create and save plots of the results
+    plot_image_results(img_1[0], img_2[0], decoded_1[0], decoded_2[0], decoded_combined[0]).savefig("model.tf/results.png")
+    plot_training_history(training_history).savefig("model.tf/accuracy.png")
 
 
 if __name__ == "__main__":
